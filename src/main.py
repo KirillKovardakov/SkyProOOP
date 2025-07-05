f80176e730e858fbe47ccd3e2ae3a6269ad6aaa3
KirillKovardakov <kir.kovardakov@gmail.com>
2025-07-06 00:31:59 +0300

    Added new classes Smartphone and LawnGrass.

--- src/main.py
+++ src/main.py
@@ -16,25 +16,24 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.name}, {self.__price} руб. Остаток:{self.quantity} шт.'
 
-    def __add__(self, other):
+    def __add__(self, other) -> float:
         """Возвращает сумму стоимости (цена * количество) двух продуктов"""
+        if type(self) is not type(other):
+            raise TypeError(f"Нельзя складывать {type(self).__name__} и {type(other).__name__}")
         return self.__price * self.quantity + other.__price * other.quantity
 
     @classmethod
-    def new_product(cls, product_data: dict, category=None):
+    def new_product(cls, product_data: dict, category: 'Category'):
         """
         Создает новый продукт или обновляет существующий в переданной категории.
         """
-        if category is None:
-            category = []
         name = product_data.get("name")
         description = product_data.get("description", "")
         price = product_data.get("price", 0.0)
         quantity = product_data.get("quantity", 0)
-
         for existing_product in category.get_product_list():
             if existing_product.name == name:
                 # Обновляем существующий продукт
@@ -66,13 +65,33 @@ class Product:
             print('Цена не должна быть нулевая или отрицательная')
 
 
+class Smartphone(Product):
+    def __init__(self, name: str, description: str, price: float, quantity: int, efficiency: float, model: str,
+                 memory: int,
+                 color: str):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    def __init__(self, name: str, description: str, price: float, quantity: int, country: str, germination_period: str,
+                 color: str):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+
 class Category:
-    __name: str
-    __description: str
-    __products: list
+    """Класс для определения категорий продуктов"""
+    __name: str = ''
+    __description: str = ''
+    __products: list = []
     category_count: int = 0
     product_count: int = 0
-    """Класс для определения категорий продуктов"""
 
     def __init__(self, name: str, description: str, products: Optional[list[Product]] = None):
         """Конструктор класса Category"""
@@ -82,12 +101,14 @@ class Category:
         self.category_count += 1
         self.product_count += len(products)
 
-    def __str__(self):
+    def __str__(self) -> str:
         all_product_count_in_category = sum([product.quantity for product in self.__products])
         return f'{self.__name}, количество продуктов: {all_product_count_in_category} шт.'
 
     def add_product(self, new_product_of_category: Optional[Product]):
         """Добавляет продукт к категории"""
+        if not isinstance(new_product_of_category, Product):
+            raise TypeError
         self.__products.append(new_product_of_category)
         self.product_count += 1
 
